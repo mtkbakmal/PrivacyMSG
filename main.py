f61458a1283bf23ce27370e5f014af9ad6bb1c6f
Alex <alexandersyrovatskij@gmail.com>
2026-04-20 00:00:29 +0600

    Fix bag with running cycle in cycle

--- main.py
+++ main.py
@@ -63,8 +63,7 @@ async def login(data: LoginSchema):
 
 async def main():
     await init_db()
-    uvicorn.run(app, host="127.0.0.1", port=8080)
-
 
 if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
     asyncio.run(main())
