a8497677e38e6f712c2fb4e37147693624f4d1db
Akmal <matkurb.akmal@icloud.com>
2026-04-19 19:17:15 +0600

    added html,css directories and connected to main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(
     title="PrivacyMSG API",
     version="1.0.0"
 )
+# Подключение статических файлов (CSS, JS)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+# Подключение html
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title":"Главная"})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.67", port=8080)
