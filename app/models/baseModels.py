19e757ac431628b40551e56a1ac7ba8da4c85201
Alex <alexandersyrovatskij@gmail.com>
2026-04-20 18:13:21 +0600

    Fix some bags

--- app/models/baseModels.py
+++ app/models/baseModels.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
 
 class RegisterSchema(BaseModel):
     username: str = Field(..., min_length=3, max_length=32, pattern=r"^[a-zA-Z0-9_]+$")
     email: EmailStr = Field(..., max_length=64)
     password: str = Field(..., min_length=8)
-    description: str = None
+    description: Optional[str] = Field(None, max_length=100)
 
 class LoginSchema(BaseModel):
     username: str = Field(..., min_length=3, max_length=32, pattern=r"^[a-zA-Z0-9_]+$")
