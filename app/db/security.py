fa5f28210584a50344c54d5bf9ab39a412b47a71
Alex <alexandersyrovatskij@gmail.com>
2026-04-20 11:33:28 +0600

    Update security.py

--- app/db/security.py
+++ app/db/security.py
@@ -1,11 +1,17 @@
-from passlib.context import CryptContext #* passlib делает даже одинаковые пароли разными в хэшированном виде
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #*Криптография крч
+import bcrypt
 
 def hash_password(password: str) -> str:
-    #!Хеширует пароль
-    return pwd_context.hash(password)
+    # Превращаем строку в байты
+    pwd_bytes = password.encode('utf-8')
+    # Генерируем соль и хешируем
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    # Возвращаем как строку для базы данных
+    return hashed.decode('utf-8')
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    #!Проверяет тот же пароль или нет
-    return pwd_context.verify(plain_password, hashed_password)
+    # Сравниваем пришедший пароль с хешем из БД
+    return bcrypt.checkpw(
+        plain_password.encode('utf-8'), 
+        hashed_password.encode('utf-8')
+    )
